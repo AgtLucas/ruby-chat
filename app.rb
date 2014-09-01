bc439e57866ec26a8195bca87a84bf8835a9f408
Lucas Churchill <lucas@agtlucas.com>
2014-09-01 16:24:44 -0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -9,6 +9,26 @@ EventMachine.run do
     end
   end
 
-  # Target here...
+  @clients = []
+
+  EM::WebSocket.start(:host => '0.0.0.0', :port => '3001') do |ws|
+    ws.onopen do |handshake|
+      @clients &lt;&lt; ws
+      ws.send "Connected to #{handshake.path}."
+    end
+
+    ws.onclose do
+      ws.send "Closed!"
+      @clients.delete ws
+    end
+
+    ws.onmessage do |msg|
+      puts "Received Message: #{msg}"
+      @clients.each do |socket|
+        socket.send msg
+      end
+    end
+  end
+
   App.run! :port => 3000
 end
